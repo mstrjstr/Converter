e458cf363af87fb4686c9067a7a13a8fe2b52917
yaproklayt <egor.rigenko828@gmail.com>
2022-12-24 00:45:38 +0500

    Добавьте файлы проекта.

---
+++ CurrencyConverter/Valute.cs
@@ -0,0 +1,22 @@
+namespace CurrencyConverter
+{
+    public class Root
+    {
+        public DateTime Date { get; set; }
+        public DateTime PreviousDate { get; set; }
+        public string PreviousURL { get; set; }
+        public DateTime Timestamp { get; set; }
+        public Dictionary<string, ValuteItem> Valute { get; set; }
+    }
+
+    public class ValuteItem
+    {
+        public string ID { get; set; }
+        public string NumCode { get; set; }
+        public string CharCode { get; set; }
+        public int Nominal { get; set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public double Previous { get; set; }
+    }
+}
